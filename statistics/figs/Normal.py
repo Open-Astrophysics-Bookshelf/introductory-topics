696085ef1eba140cf5080c3ecf233a5b6d06eea4
Edward Brown <ebrown@pa.msu.edu>
2016-01-24 09:41:40 -0500

    better figures for normal distribution

--- statistics/figs/Normal.py
+++ statistics/figs/Normal.py
@@ -26,15 +26,16 @@ plt.xlim(x0,x1)
 plt.ylim(y0,y1)
 
 # some sizing to make the plot look nice
-charsize = 10   # points
-major_ticklength = 0.6*charsize
+charsize = 12   # points
+labelsize=10
+major_ticklength = 0.6*labelsize
 major_tickwidth = 0.9   # points
-minor_ticklength = 0.3*charsize
+minor_ticklength = 0.3*labelsize
 minor_tickwidth = 0.7   # points
 plt.tight_layout(pad=1.2)
 
 plt.tick_params(axis='both',\
-    length=major_ticklength, width=major_tickwidth,which='major',labelsize=10)
+    length=major_ticklength, width=major_tickwidth,which='major',labelsize=labelsize)
 plt.tick_params(axis='both',\
     length=minor_ticklength,width=minor_tickwidth,which='minor',labelsize=8)
 
@@ -42,14 +43,14 @@ plt.ylabel(r'$p(x;\mu,\sigma)$',fontsize=charsize)
 plt.xlabel(r'$x$',fontsize=charsize)
 plt.title(r'$\mu = 0.0$',fontsize=charsize)
 
-for sigma,clr in zip([1.0,2.0,3.0],['k','r','b']):
+for sigma,clr,lns in zip([1.0,2.0,3.0],['0.5','0.3','0.1'],['-','--',':']):
     rv = norm(loc=mu,scale=sigma)
     y = rv.pdf(x)
     plt.plot(x,y,\
-        linestyle='-',linewidth=2.0,solid_capstyle='round',\
+        linestyle=lns,linewidth=2.0,solid_capstyle='round',\
         solid_joinstyle='miter',color=clr)
 
-plt.legend((r'$\sigma=1.0$',r'$\sigma=2.0$',r'$\sigma=3.0$'),loc='best',fontsize=charsize)
+plt.legend((r'$\sigma=1.0$',r'$\sigma=2.0$',r'$\sigma=3.0$'),loc='best',fontsize=labelsize)
 plt.savefig('Normal_mu0.pdf',format='pdf')
 plt.clf()
 
@@ -68,13 +69,13 @@ plt.ylabel(r'$p(x;\mu,\sigma)$',fontsize=charsize)
 plt.xlabel(r'$x$',fontsize=charsize)
 plt.title(r'$\sigma = 1.0$',fontsize=charsize)
 
-for mu,clr in zip([0.0,2.0,4.0],['k','r','b']):
+for mu,clr,lns in zip([0.0,2.0,4.0],['0.5','0.3','0.1'],['-','--',':']):
     rv = norm(loc=mu,scale=1.0)
     y = rv.pdf(x)
     plt.plot(x,y,\
-        linestyle='-',linewidth=2.0,solid_capstyle='round',\
+        linestyle=lns,linewidth=2.0,solid_capstyle='round',\
         solid_joinstyle='miter',color=clr)
 
-plt.legend((r'$\mu=0.0$',r'$\mu=2.0$',r'$\mu=4.0$'),loc='best',fontsize=charsize)
+plt.legend((r'$\mu=0.0$',r'$\mu=2.0$',r'$\mu=4.0$'),loc='best',fontsize=labelsize)
 plt.savefig('Normal_sigma1.pdf',format='pdf')
 
