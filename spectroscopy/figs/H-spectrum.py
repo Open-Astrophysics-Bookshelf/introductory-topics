b78a1af98a6b621ab3e8f6f0b1791c6a77161c6f
Edward Brown <ebrown@pa.msu.edu>
2017-02-24 15:36:37 -0500

    more details on spectrum figure

--- spectroscopy/figs/H-spectrum.py
+++ spectroscopy/figs/H-spectrum.py
@@ -13,7 +13,7 @@ height = 1.2
 plt.style.use('text-sans')
 plt.rc('xtick',**{'direction':'out'})
 
-Nlines = 5
+Nlines = 50
 greek = [r'$\alpha$',r'$\beta$',r'$\gamma$',r'$\delta$',r'$\epsilon$']
 
 Lyman = [ wavelength(1,m) for m in range(2,2+Nlines) ]
@@ -23,18 +23,28 @@ Paschen = [ wavelength(3,m) for m in range(4,4+Nlines) ]
 plt.figure(figsize=(width,height))
 plt.gca().get_yaxis().set_visible(False)
 ymin,ymax = plt.ylim(0,1)
-plt.xlim(20,1400)
-plt.vlines(Lyman,ymin,ymax,color='0.5',linestyle='--')
+plt.xlim(20,1500)
+plt.vlines(Lyman,ymin,ymax,color='0.5',linestyle='-',linewidth=0.5)
 plt.annotate(s=r'Lyman ($m\to 1$)',fontsize='small',xytext=(0,3),
     textcoords='offset points',xy=(Lyman[-1],0),
     va='bottom',ha='right',rotation=90)
-plt.vlines(Balmer,ymin,ymax,color='0.5',linestyle=':')
+plt.vlines(Balmer,ymin,ymax,color='0.5',linestyle='-',linewidth=0.5)
 plt.annotate(s=r'Balmer ($m\to 2$)',fontsize='small',xytext=(0,3),
     textcoords='offset points',xy=(Balmer[-1],0),
     va='bottom',ha='right',rotation=90)
-plt.vlines(Paschen,ymin,ymax,color='0.5',linestyle='-')
+plt.annotate(s=r'$m = 3$',fontsize='x-small',xy=(Balmer[0],0.5),
+    va='center',ha='center')
+plt.annotate(s=r'$m = 4$',fontsize='x-small',xy=(Balmer[1],0.5),
+    va='center',ha='center')
+
+plt.vlines(Paschen,ymin,ymax,color='0.5',linestyle='-',linewidth=0.5)
 plt.annotate(s=r'Paschen ($m\to 3$)',fontsize='small',xytext=(0,3),
     textcoords='offset points',xy=(Paschen[-1],0),
     va='bottom',ha='right',rotation=90)
+plt.annotate(s=r'$m = 5$',fontsize='x-small',xy=(Paschen[1],0.5),
+    va='center',ha='center')
+plt.annotate(s=r'$m = 6$',fontsize='x-small',xy=(Paschen[2],0.5),
+    va='center',ha='center')
+
 plt.xlabel(r'$\lambda$ (nm)')
 plt.savefig('H-spectrum.pdf',format='pdf',bbox_inches='tight')
